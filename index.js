277de5f8b0cb686a4cb338401f91e32179560145
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2025-04-25 14:42:05 +0700

    change port

--- index.js
+++ index.js
@@ -72,5 +72,5 @@ app.get("/profile", authenticate, async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
